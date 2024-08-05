fde9fac8cdb27af45f2f3c146b5018dd8614b1ee
1Cyanide1 <vanoni911@gmail.com>
2024-08-05 13:51:42 +0300

    Some features for django admin and new models added to admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import FoodCategory, Food
 
-# Register your models here.
+
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name_ru', 'name_en', 'name_ch', 'order_id')
+    search_fields = ('name_ru', 'name_en', 'name_ch')
+    list_filter = ('order_id',)
+    ordering = ('order_id', 'name_ru')
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name_ru', 'category', 'is_publish', 'is_vegan', 'is_special', 'cost')
+    search_fields = ('name_ru', 'description_ru')
+    list_filter = ('category', 'is_vegan', 'is_special', 'is_publish')
+    ordering = ('category', 'name_ru')
+    autocomplete_fields = ('additional',)
